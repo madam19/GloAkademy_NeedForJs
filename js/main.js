2dbd2af4a279ccfbfb3ca81bfb45e42edc2fe93e
madam19 <85499579+madam19@users.noreply.github.com>
2021-06-23 19:26:39 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const score = document.querySelector('.score'),
+    start = document.querySelector('.start'),
+    gameArea = document.querySelector('.gameArea'),
+    car = document.createElement('div');
+car.classList.add('car');
+
+
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyup', stopRun);
+
+const keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowRigth: false,
+    ArrowLeft: false
+
+};
+const setting = {
+    start: false,
+    score: 0,
+    speed: 3
+};
+
+function startGame() {
+    start.classList.add('hide');
+    setting.start = true;
+    gameArea.appendChild(car);
+    requestAnimationFrame(playGame);
+}
+
+function playGame() {
+    console.log('Play game!');
+    if (setting.start) {
+        requestAnimationFrame(playGame);
+    }
+
+}
+
+function startRun(event) {
+    event.preventDefault();
+    keys[event.key] = true;
+
+
+}
+
+function stopRun(event) {
+    event.preventDefault();
+    setting.start = false;
+    keys[event.key] = false;
+}
