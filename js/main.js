b9f373074b058991d17ad83a89a84abfcb6af148
madam19 <85499579+madam19@users.noreply.github.com>
2021-06-24 21:21:53 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 'use strict';
 
+
 const score = document.querySelector('.score'),
     start = document.querySelector('.start'),
     gameArea = document.querySelector('.gameArea'),
     car = document.createElement('div');
-car.classList.add('car');
-
-
-start.addEventListener('click', startGame);
-document.addEventListener('keydown', startRun);
-document.addEventListener('keyup', stopRun);
 
 const keys = {
     ArrowUp: false,
@@ -21,23 +16,72 @@ const keys = {
 const setting = {
     start: false,
     score: 0,
-    speed: 3
+    speed: 3,
+    traffic: 3
 };
 
-function startGame() {
-    start.classList.add('hide');
-    setting.start = true;
-    gameArea.appendChild(car);
-    requestAnimationFrame(playGame);
+function moveRoad() {
+    let lines = document.querySelectorAll('.line');
+    lines.forEach(function(line) {
+        line.y += setting.speed;
+        line.style.top = line.y + 'px';
+        if (line.y > document.documentElement.clientHeight) {
+            line.y = -100;
+
+        }
+    });
 }
 
 function playGame() {
-    console.log('Play game!');
+
     if (setting.start) {
+        moveRoad();
+        if (keys.ArrowLeft && setting.x > 0) { //изменение координат, если нажата клавиша
+            setting.x -= setting.speed;
+        }
+        if (keys.ArrowRigth && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeigth - car.offsetHeight)) {
+            setting.y += setting.speed;
+        }
+        if (keys.ArrowUP && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+
+        car.style.left = setting.x + 'px'; // передаем изменные данные
+        car.style.top = setting.y + 'px';
         requestAnimationFrame(playGame);
     }
 
 }
+
+function getQuantityElements(heigthElement) {
+    return document.documentElement.clientHeight / heigthElement + 1;
+
+}
+
+function startGame() {
+    start.classList.add('hide'); //начало игры, убирает запись с экрана
+
+    for (let i = 0; i < getQuantityElements(100); i++) {
+        const line = document.createElement('div');
+        line.classList.add('line');
+        line.style.top = (i * 100) + 'px';
+        line.y = i * 100 + 'px';
+        gameArea.appendChild(line);
+
+
+    }
+    setting.start = true;
+    gameArea.appendChild(car); // car
+    setting.x = car.offsetLeft; // positon car
+    setting.y = car.offsetTop;
+    requestAnimationFrame(playGame);
+}
+
+
+
 
 function startRun(event) {
     event.preventDefault();
@@ -48,6 +92,15 @@ function startRun(event) {
 
 function stopRun(event) {
     event.preventDefault();
-    setting.start = false;
     keys[event.key] = false;
 }
+
+
+
+
+
+
+car.classList.add('car');
+start.addEventListener('click', startGame);
+document.addEventListener('keydown', startRun);
+document.addEventListener('keyesc', stopRun);
